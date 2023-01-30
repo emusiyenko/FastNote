9e87dba10baa6b99224338dd0578b862bc2a89d9
emusiienko <emusiienko@waverleysoftware.com>
2023-01-30 11:54:20 +0200

    adopted for aws

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from .routers import auth, notes
+from mangum import Mangum
 
 app = FastAPI()
 app.include_router(auth.router)
 app.include_router(notes.router)
+
+handler = Mangum(app)
