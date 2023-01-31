4b21e528bae2f262f242a2810a74b21c56363d8b
emusiienko <emusiienko@waverleysoftware.com>
2023-01-31 11:57:49 +0200

    added root path

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import os
 from fastapi import FastAPI
 from .routers import auth, notes
 from mangum import Mangum
+root_path = os.environ.get('API_ROOT_PATH')
 
-app = FastAPI()
+app = FastAPI(root_path=root_path)
 app.include_router(auth.router)
 app.include_router(notes.router)
 
