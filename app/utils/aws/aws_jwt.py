b35ea8e5d4eabd401bdfead739e2619b1f1340c0
Eugene <emusiienko@planful.com>
2023-02-23 11:13:36 +0200

    applied cognitojwt

--- app/utils/aws/aws_jwt.py
+++ app/utils/aws/aws_jwt.py
@@ -1,15 +1,10 @@
-import requests
 import boto3
 import logging
-from typing import Dict, List
-from jose import jwt, jwk
-from jose.utils import base64url_decode
+import cognitojwt
 from app.settings import Settings
 from app.schemas import AWSIdentity
 from .aws_exception import AWSServicesException
 
-JWK = Dict[str, str]
-JWKS = Dict[str, List[JWK]]
 settings = Settings()
 
 
@@ -20,7 +15,7 @@ def get_aws_identity(token: str) -> AWSIdentity:
         id_response = identity_client.get_id(AccountId=settings.aws_account_id,
                                              IdentityPoolId=settings.cognito_identity_pool_id,
                                              Logins={
-                                                user_pool_full_identifier: token
+                                                 user_pool_full_identifier: token
                                              })
         identity_id = id_response['IdentityId']
         credentials = identity_client.get_credentials_for_identity(IdentityId=identity_id, Logins={
@@ -41,7 +36,8 @@ def get_aws_identity(token: str) -> AWSIdentity:
         raise AWSServicesException(recommended_status_code=500, detail=repr(ex))
 
     try:
-        verified, claims = _verify_jwt(token, _get_jwks())
+        claims = cognitojwt.decode(token, settings.aws_region, settings.cognito_user_pool_id,
+                                   settings.cognito_client_id, testmode=False)
         identity_object = AWSIdentity.parse_obj(credentials)
         identity_object.cognito_claims = claims
     except Exception as ex:
@@ -49,31 +45,3 @@ def get_aws_identity(token: str) -> AWSIdentity:
 
     return identity_object
 
-
-def _get_jwks():
-    jwks_url = f'https://cognito-idp.{settings.aws_region}.amazonaws.com' \
-               f'/{settings.cognito_user_pool_id}/.well-known/jwks.json'
-    return requests.get(jwks_url).json()
-
-
-def _get_hmac_key(token: str, jwks: JWKS):
-    kid = jwt.get_unverified_header(token).get("kid")
-    for key in jwks.get("keys", []):
-        if key.get("kid") == kid:
-            return key
-
-
-def _verify_jwt(token: str, jwks: JWKS):
-    hmac_key = _get_hmac_key(token, jwks)
-
-    if not hmac_key:
-        raise ValueError("No pubic key found!")
-
-    hmac_key = jwk.construct(hmac_key)
-    claims = jwt.get_unverified_claims(token)
-
-    message, encoded_signature = token.rsplit(".", 1)
-    decoded_signature = base64url_decode(encoded_signature.encode())
-    verified = hmac_key.verify(message.encode(), decoded_signature)
-
-    return verified, claims
