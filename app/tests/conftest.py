8dd4071263530a485746795db41676d145660bae
Eugene <emusiienko@planful.com>
2023-03-23 13:27:51 +0200

    first unit test with dynamodb

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -2,12 +2,38 @@ import os
 import boto3
 import pytest
 import json
+import importlib
 
 from pathlib import Path
 from dotenv import load_dotenv
-from moto import mock_cognitoidp, mock_cognitoidentity
-from ..settings import Settings
+from moto import mock_cognitoidp, mock_dynamodb
+from app.settings import Settings
 from fastapi.testclient import TestClient
+from moto.cognitoidentity.models import CognitoIdentityBackend
+from moto.core.base_backend import BackendDict
+from moto.core.models import base_decorator
+from moto.cognitoidentity.urls import url_bases, url_paths
+
+"""
+Default backend generates new identity each time it's requested by get_id.
+On the other hand identity_id is the hash key in dynamo and must be stable for a single user.
+The code below replaces the default backend by a custom one
+"""
+
+
+class StaticCognitoIdentityBackend(CognitoIdentityBackend):
+    def get_id(self, identity_pool_id: str) -> str:
+        identity_id = f"{self.region_name}:123456789"
+        identity_id_dict = {"IdentityId": identity_id}
+        if not len(self.pools_identities[identity_pool_id]["Identities"]):
+            self.pools_identities[identity_pool_id]["Identities"].append(identity_id_dict)
+        return json.dumps(identity_id_dict)
+
+
+cognitoidentity_backends = BackendDict(StaticCognitoIdentityBackend, "cognito-identity")
+cognito_responses_module = importlib.import_module('moto.cognitoidentity.responses')
+setattr(cognito_responses_module, 'cognitoidentity_backends', cognitoidentity_backends)
+setattr(cognito_responses_module, 'CognitoIdentityBackend', StaticCognitoIdentityBackend)
 
 
 @pytest.fixture(scope="function")
@@ -19,24 +45,26 @@ def aws_credentials():
 @pytest.fixture(scope="function")
 def cognito_idp(aws_credentials):
     with mock_cognitoidp():
-        with mock_cognitoidentity():
-            settings = Settings()
-            cognito_service = boto3.client('cognito-idp')
-            user_pool_id = cognito_service.create_user_pool(PoolName='test_pool')["UserPool"]["Id"]
-            user_pool_client_id = cognito_service.create_user_pool_client(
-                UserPoolId=user_pool_id, ClientName='test_client', CallbackURLs=['test_callback']
-            )['UserPoolClient']['ClientId']
-            cognito_service.create_group(GroupName=settings.cognito_regular_user_group_name,
-                                         UserPoolId=user_pool_id)
-            os.environ['COGNITO_USER_POOL_ID'] = user_pool_id
-            os.environ['COGNITO_CLIENT_ID'] = user_pool_client_id
+        mock = base_decorator(cognitoidentity_backends)()
+        mock.start()
+        settings = Settings()
+        cognito_service = boto3.client('cognito-idp')
+        user_pool_id = cognito_service.create_user_pool(PoolName='test_pool')["UserPool"]["Id"]
+        user_pool_client_id = cognito_service.create_user_pool_client(
+            UserPoolId=user_pool_id, ClientName='test_client', CallbackURLs=['test_callback']
+        )['UserPoolClient']['ClientId']
+        cognito_service.create_group(GroupName=settings.cognito_regular_user_group_name,
+                                     UserPoolId=user_pool_id)
+        os.environ['COGNITO_USER_POOL_ID'] = user_pool_id
+        os.environ['COGNITO_CLIENT_ID'] = user_pool_client_id
 
-            cognito_identity_service = boto3.client('cognito-identity')
-            response = cognito_identity_service.create_identity_pool(IdentityPoolName='test_identity_pool',
-                                                                     AllowUnauthenticatedIdentities=False)
-            os.environ['COGNITO_IDENTITY_POOL_ID'] = response['IdentityPoolId']
+        cognito_identity_service = boto3.client('cognito-identity')
+        response = cognito_identity_service.create_identity_pool(IdentityPoolName='test_identity_pool',
+                                                                 AllowUnauthenticatedIdentities=False)
+        os.environ['COGNITO_IDENTITY_POOL_ID'] = response['IdentityPoolId']
 
-            yield cognito_service, user_pool_id
+        yield cognito_service, user_pool_id
+        mock.stop()
 
 
 @pytest.fixture(scope="function")
@@ -67,9 +95,42 @@ def cognito_idp_with_confirmed_user(cognito_idp_with_new_user):
 
 @pytest.fixture(scope="function")
 def logged_in_client(client, cognito_idp_with_confirmed_user):
+    from app.auth.aws_jwt import get_aws_identity
     cognito_service, user_pool_id, username, password = cognito_idp_with_confirmed_user
     response = client.post(f'/auth/sign_in', data={"username": username, "password": password})
     assert response.status_code == 200
     body = json.loads(response.content)
     access_token = body['access_token']
-    return client, {"Authorization": f"Bearer {access_token}"}
+    identity = get_aws_identity(access_token)
+    return client, {"Authorization": f"Bearer {access_token}"}, identity
+
+
+@pytest.fixture(scope="function")
+def dynamo_db_table(aws_credentials):
+    with mock_dynamodb():
+        settings = Settings()
+        dynamodb_resource = boto3.resource('dynamodb')
+        dynamodb_resource.create_table(TableName=settings.dynamo_db_notes_table,
+                                       AttributeDefinitions=[
+                                           {
+                                               'AttributeName': 'user_id',
+                                               'AttributeType': 'S'
+                                           },
+                                           {
+                                               'AttributeName': 'contents',
+                                               'AttributeType': 'S'
+                                           }
+                                       ],
+                                       KeySchema=[
+                                           {
+                                               'AttributeName': 'user_id',
+                                               'KeyType': 'HASH'
+                                           },
+                                           {
+                                               'AttributeName': 'contents',
+                                               'KeyType': 'RANGE'
+                                           }
+                                       ],
+                                       BillingMode='PAY_PER_REQUEST')
+        table = dynamodb_resource.Table(settings.dynamo_db_notes_table)
+        yield table
--- app/tests/test_notes.py
+++ app/tests/test_notes.py
@@ -1,3 +1,6 @@
+import json
+
+
 class TestNotes:
 
     @staticmethod
@@ -6,7 +9,21 @@ class TestNotes:
         assert response.status_code == 401
 
     @staticmethod
-    def test_get_notes(logged_in_client):
-        client, headers = logged_in_client
+    def test_get_notes(logged_in_client, dynamo_db_table):
+        from app.utils.dynamodb_service import DynamoDBNote
+        client, headers, identity = logged_in_client
+
+        dynamodb_note = DynamoDBNote(identity.identity_id,
+                                     contents='note_test',
+                                     title='test_title', text='test_text')
+        dynamodb_note.save()
+
         response = client.get('/notes', headers=headers)
         assert response.status_code == 200
+
+        notes = json.loads(response.content)
+        assert len(notes) == 1
+
+        note = notes[0]
+        assert note['title'] == dynamodb_note.title and note['text'] == dynamodb_note.text and note['note_id'] == 'test'
+
